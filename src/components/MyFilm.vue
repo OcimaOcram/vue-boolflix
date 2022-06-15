8ed437ffa385ebf274bb187cc54e02697ea3e5e6
OcimaOcram <marco.amico.pesci00@gmail.com>
2022-06-15 12:52:01 +0200

    milestone 2 completata

--- src/components/MyFilm.vue
+++ src/components/MyFilm.vue
@@ -1,10 +1,12 @@
 <template>
   
     <div>
-        <section id="searchbar"><input type="text" placeholder="search" v-model="ricercaUtente"> <button @click="searchFilm">cerca</button></section>
+        <section id="searchbar"><input type="text" placeholder="search" v-model="ricercaUtente"> <button @click="searchMovie">cerca</button></section>
         <MyCard
-        v-for="(element, i) in listaFilm" :key="i" :filmDescription="element"
+        v-for="element in listaFilm" :key="element.i" :filmDescription="element"
         />
+        <MySeries  v-for="item in listaSerie" :key="item.index" :seriesDescription="item"/>
+        
 
     </div>
 
@@ -14,13 +16,15 @@
 <script>
     import axios from 'axios'
     import MyCard from './MyCard.vue';
+    import MySeries from './MySeries.vue';
+    
     export default {
     name: "MyFilm",
     data() {
         return {
             apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=573c6074892e33f67b46537c63bfbe2c&language=it-IT&query=a",
             apiUrlTv:"https://api.themoviedb.org/3/search/tv?api_key=573c6074892e33f67b46537c63bfbe2c&language=it-IT&query=a",
-            ListaSerie: [],
+            listaSerie: [],
             listaFilm: [],
             ricercaUtente: ""
         };
@@ -32,36 +36,31 @@
             console.log(result);
         });
     },
-    components: { 
-        MyCard 
-        },
+    components: {
+    MyCard,
+    MySeries
+},
     
     methods: {
-        searchSeries() {
-            console.log(this.ricercaUtente);
-            this.apiUrlTv= "https://api.themoviedb.org/3/search/tv?api_key=573c6074892e33f67b46537c63bfbe2c&language=it-IT&query=" + this.ricercaUtente;
-            console.log(this.apiUrlTv);
-            axios.get(this.apiUrlTv)
-            .then(result => {
-            this.ListaSerie = result.data.results;
-            console.log(result);
-            });
-            
-        },
-        searchFilm() {
-            console.log(this.ricercaUtente);
-            this.apiUrl= "https://api.themoviedb.org/3/search/movie?api_key=573c6074892e33f67b46537c63bfbe2c&language=it-IT&query=" + this.ricercaUtente;
-            console.log(this.apiUrl);
-            axios.get(this.apiUrl)
-            .then(result => {
-            this.listaFilm = result.data.results;
-            console.log(this.ListaSerie);
-            });
-             this.searchSeries();
-            
-        },
-        
+    searchMovie() {
+      this.apiUrl= "https://api.themoviedb.org/3/search/movie?api_key=573c6074892e33f67b46537c63bfbe2c&language=it-IT&query=" + this.ricercaUtente;
+      axios.get(this.apiUrl)
+      .then(result => {
+          this.listaFilm = result.data.results;
+          console.log(result)
+      }),
+      this.searchSerie()
+    },
+    searchSerie() {
+      this.apiUrlTv= "https://api.themoviedb.org/3/search/tv?api_key=573c6074892e33f67b46537c63bfbe2c&language=it-IT&query=" + this.ricercaUtente;
+      axios.get(this.apiUrlTv)
+      .then(result => {
+          this.listaSerie = result.data.results;
+          console.log(result)
+      })
     }
+
+  }
 }
     
 
---
+++ src/components/MySeries.vue
@@ -0,0 +1,37 @@
+<template>
+   <div class="container">
+    <ul>
+        <li>
+            <h1>{{seriesDescription.name}}</h1>
+            <h2>{{seriesDescription.original_name}}</h2>
+            <h3><lang-flag :iso="seriesDescription.original_language"/></h3>
+            <h4>{{seriesDescription.vote_average}}</h4>
+        </li>
+    </ul>
+   </div>
+
+
+
+</template>
+
+<script>
+
+export default {
+  name: 'MySeries',
+  
+  props: {
+    seriesDescription: Object
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+li {
+  display: inline-block;
+  margin: 15px;
+}
+
+</style>
